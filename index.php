b31c94fb3b20580724067640ed6248c24fcff602
Afvanie <95737735+Afvanie@users.noreply.github.com>
2022-11-25 12:53:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>KBTI2021</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="assets/w3.css">
+<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<script src="assets/w3.js"></script>
+</head>
+<body class="w3-light-gray">
+
+
+<br>
+<div class="w3-container w3-light-gray">
+	<div class="w3-row">
+    	<div class="w3-col l1">&nbsp;</div>
+        <div class="w3-col l10 w3-white">
+        	<div class="w3-container"><br>
+            	<div class="w3-row">
+                	<div class="w3-col l12 w3-center">
+                    	<div class="w3-xlarge">APLIKASI SISTEM PAKAR</div>
+                        <div class="w3-large"><strong>DIAGNOSIS KERUSAKAN MOBIL</strong></div>
+                        <div class="w3-small" style="margin-top:3px;">MENGGUNAKAN METODE <strong>FORWARD CHAINING</strong></div>
+                    </div>
+                    
+                </div>
+                <hr>
+            </div>
+            
+            <div class="w3-container">
+            	<div class="w3-row">
+                	<div class="w3-col l3">
+                    	<div>
+                        	<ul class="w3-ul w3-hoverable w3-border">
+  <a href="index.php" style="text-decoration:none"><li style="border-bottom:1px solid #DDD;"><i class="fa fa-home fa-fw"></i> Beranda</li></a>
+  
+  <a href="diagnosa_create.php" style="text-decoration:none"><li style="border-bottom:1px solid #DDD;"><i class="fa fa-book fa-fw"></i> Diagnosa</li></a>
+  
+  <a href="penyakit_read.php" style="text-decoration:none"><li style="border-bottom:1px solid #DDD;"><i class="fa fa-book fa-fw"></i> Daftar Kerusakan</li></a>
+
+  <a href="admin/index.php" style="text-decoration:none"><li style="border-bottom:1px solid #DDD;"><i class="fa fa-book fa-fw"></i> Login </li></a>
+  
+  <!-- <a href="informasi_read.php" style="text-decoration:none"><li style="border-bottom:1px solid #DDD;"><i class="fa fa-book fa-fw"></i> Seputar Kerusakan Mobil</li></a> -->
+  
+  
+</ul>
+                        </div>
+                    </div>
+                    <div class="w3-col l9" style="padding-left:8px;">
+                    	<div class="w3-border w3-padding">
+                        <div class="w3-border-bottom" style="padding-bottom:10px; margin-top:8px;"><strong><i class="fa fa-home"></i> BERANDA</strong></div>
+                        
+                        <div class="w3-center"><img src="assets/Logo BJM.png" width="384" height="247"></div>
+                        <div class="w3-center">
+                        <div class="w3-xlarge">SELAMAT DATANG DI</div>
+                        	<div class="w3-large">SISTEM PAKAR DIAGNOSIS KERUSAKAN MOBIL</div>
+                            <div class="w3-medium">Silahkan Pilih <strong>Menu Diagnosa</strong> Untuk Mengetahui Kerusakan yang Anda Alami</div>
+                        </div>
+                        
+                        <br>
+
+                        
+                        </div>
+                    
+                    </div>
+                </div>
+            </div><br>
+
+        </div>
+        <div class="w3-col l1">&nbsp;</div>
+    </div>
+</div><br>
+<br>
+<div class="w3-center w3-small">Copyright @ 2022 <strong>BJM BENGKEL</strong><br>
+All Right Reserved</div>
+<br>
+
+	
+</body>
+</html>
